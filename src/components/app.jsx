ec1f96b16078aaade08d73a0dce134f0f9cc0ff0
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-07-27 21:41:46 +0200

    testing react, lesson outdated

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return {
-      <h1>"Hello World"</h1>
+      <div>
+        <h1>
+        "Hello World"
+        </h1>
+      </div>
     };
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,7 @@ import App from './components/app.jsx';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<App />, root);
+  ReactDOM.render(
+  <App />,
+  root);
 }
